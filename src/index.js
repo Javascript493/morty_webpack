0735c46b70f01b8903a304a40928529ba9cab94a
JavaScript493 <984718946@qq.com>
2024-07-03 14:56:36 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 async function getComponent() {
   // lodash 现在使用 import 引入
-  const {default: _} = await import('loadsh')
+  const {default: _} = await import('lodash')
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   return element;
