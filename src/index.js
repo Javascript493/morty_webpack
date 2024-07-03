f1a3eab7cc276019613d49b37799a469b0126f19
JavaScript493 <984718946@qq.com>
2024-07-03 15:56:09 +0800

    fea: 开发环境

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-async function getComponent() {
-  // lodash 现在使用 import 引入
-  const {default: _} = await import('lodash')
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
- }
+import _ from 'lodash'
+import printMe from './print'
+import Icon from './icon.png'
+function component() {
+  const element = document.createElement('div')
+  const btn = document.createElement('button')
+  element.innerHTML = _.join(['hello', 'webpack'], ' ')
+  btn.innerHTML = 'click me and check the console!'
+  btn.onclick = printMe
+  element.appendChild(btn)
+  const myIcon = new Image();
+  myIcon.src = Icon;
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+  element.appendChild(myIcon);
+  return element
+}
+document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -5,58 +5,32 @@ module.exports = {
   entry: {
     // 多入口
     index: './src/index.js',
+    print: './src/print.js'
+  },
+  devServer: {
+    static: './dist'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '管理输出'
+      title: '开发环境-development'
     })
   ],
+  devtool: 'inline-source-map',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
+    publicPath: '/'
   },
+  optimization: {
+    runtimeChunk: 'single'
+  },
+  module: {
+    rules:[
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
 }
-
-
-// 防止重复
-// const path = require('path')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
-// module.exports = {
-//   mode: 'development',
-//   entry: {
-//     // 多入口
-//     index: './src/index.js',
-//     another: './src/another-module.js'
-//     // 防止重复
-//     // index: {
-//     //   import: './src/index.js',
-//     //   dependOn: 'shared'
-//     // },
-//     // another: {
-//     //   import: './src/another-module.js',
-//     //   dependOn: 'shared',
-//     // },
-//     // shared: 'lodash'
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       title: '管理输出'
-//     })
-//   ],
-//   output: {
-//     /**
-//      * 此时 如果index和another-module共同引用了lodash 打包后的bundle 都会重复的引用
-//      * 解决这个问题可以采用 dependOn 选项
-//      */
-//     filename: '[name].bundle.js',
-//     path: path.resolve(__dirname, 'dist'),
-//     clean: true
-//   },
-//   optimization: {
-//     // runtimeChunk: 'single'
-//     splitChunks: {
-//       chunks: 'all'
-//     }
-//   }
-// }
